1c39da9943d26e00939af7c450557a60e0dbcad1
Davide Madrisan <davide.madrisan@gmail.com>
2018-05-02 00:18:25 +0200

    Bump to version 2
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- vault_ha_active_node.py
+++ vault_ha_active_node.py
@@ -1,47 +1,90 @@
 #!/usr/bin/env python3
 
-"""Tracking script for HashiCorp Vault HA Cluster.
+'''Keepalived Tracking Script for HashiCorp Vault HA.
 
-This simple Python script can be used as a tracking script for monitoring
-an HashiCorp Vault HA cluster and setup a VIP on the active Vault node.
+This simple Python script can be used as a Keepalived tracking script for
+monitoring an HashiCorp Vault HA cluster and setup a VIP on the active Vault
+node.
 
 GitHub Site:
     https://github.com/madrisan/keepalived-vault-ha
-"""
+
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+'''
 
 __author__ = "Davide Madrisan <davide.madrisan.gmail.com>"
-__copyright__ = "Copyright 2018 Davide Madrisan"
+__copyright__ = "Copyright (C) 2018 Davide Madrisan"
 __license__ = "GPL-3.0"
-__status__ = "Stable"
-__version__ = "1"
+__status__ = "Production"
+__version__ = "2"
 
-from os import getenv
+import argparse
+import logging
+import os
 import requests
 import sys
 
-def main():
+def parse_args():
+    '''This function parses and return arguments passed in'''
+    progname = os.path.basename(sys.argv[0])
+    parser = argparse.ArgumentParser(
+        description = 'Keepalived Tracking Script for HashiCorp Vault HA.')
+    parser.add_argument(
+        "-d", "--debug",
+        action = "store_true",
+        help = "execute this script in debug mode",
+        dest = "debug")
+    parser.add_argument(
+        "-t", "--timeout",
+        help = "stop waiting for a response after a given number of seconds",
+        dest = "timeout",
+        default=3,
+        type=float)
+    parser.add_argument(
+        "--version", action = "version",
+        version = "{0} v{1}"
+                  " ( https://github.com/madrisan/keepalived-vault-ha )"
+                  .format(progname, __version__))
+
+    return parser.parse_args()
+
+def check_vault(timeout):
+    '''This function returns True if the node pointed by the environment variable
+       VAULT_ADDR is active, False otherwise.'''
+
     api_version = 'v1'
     resource = 'sys/leader'
 
-    vault_addr = getenv('VAULT_ADDR', '')
+    vault_addr = os.getenv('VAULT_ADDR', '')
     if not vault_addr:
-        sys.exit(1)
+        logging.debug('VAULT_ADDR is unset, aborting...')
+        return False
 
-    leader_url = '{0}/{1}/{2}'.format(vault_addr, api_version, resource)
+    logging.debug('VAULT_ADDR: {0}'.format(vault_addr))
+
+    leader_url = ('{0}/{1}/{2}'
+                  .format(vault_addr, api_version, resource))
+    logging.debug('Querying the URL: {0}'.format(leader_url))
 
     try:
-        r = requests.get(leader_url)
+        r = requests.get(leader_url, timeout=timeout)
         if r.status_code != 200:
-            sys.exit(1)
-
+            logging.debug('requests returned with status code {0}'.format(r.status_code))
+            return False
         ha_enabled = r.get('ha_enabled', False)
         is_self = r.get('is_self', False)
         if ha_enabled and is_self:
-            sys.exit(0)
-    except:
-        pass
+            return True
+    except requests.exceptions.RequestException as e:
+        logging.error(format(str(e)))
 
-    sys.exit(1)
+    return False
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    if args.debug:
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+    is_active = check_vault(args.timeout)
+    sys.exit(0 if is_active else 1)
