a24f45d98dda8a6a3868c9aa77b1a79fba5ba5ff
Davide Madrisan <davide.madrisan@gmail.com>
2018-05-01 11:49:25 +0200

    Fix return codes
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- vault-ha-active-node.py
+++ vault-ha-active-node.py
@@ -2,6 +2,7 @@
 
 from os import getenv
 import requests
+import sys
 
 def main():
     api_version = 'v1'
@@ -9,23 +10,23 @@ def main():
 
     vault_addr = getenv('VAULT_ADDR', '')
     if not vault_addr:
-        return 1
+        sys.exit(1)
 
     leader_url = '{0}/{1}/{2}'.format(vault_addr, api_version, resource)
 
     try:
         r = requests.get(leader_url)
         if r.status_code != 200:
-            return 1
+            sys.exit(1)
 
         ha_enabled = r.get('ha_enabled', False)
         is_self = r.get('is_self', False)
         if ha_enabled and is_self:
-            return 0
+            sys.exit(0)
     except:
         pass
 
-    return 1
+    sys.exit(1)
 
 if __name__ == '__main__':
     main()
